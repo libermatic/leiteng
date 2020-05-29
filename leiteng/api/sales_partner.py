f209c50b753e93d89a26176059a0e111734b8ab1
Sun Howwrongbum <sun@libermatic.com>
2020-05-29 09:56:51 +0000

    feat: add partner job methods

--- leiteng/api/sales_partner.py
+++ leiteng/api/sales_partner.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import frappe
-from toolz import merge
+from toolz.curried import merge, groupby, valmap, compose, concat, map, filter
 
 from leiteng.app import get_decoded_token, auth
 from leiteng.utils import pick, handle_error
@@ -112,3 +112,166 @@ def generate_signup_code(sales_partner_name):
         "Sales Partner", sales_partner_name, "le_sign_up_code", sign_up_code
     )
     return sign_up_code
+
+
+@frappe.whitelist(allow_guest=True)
+@handle_error
+def get_job_list(token, page="1", page_length="10", status=None):
+    decoded_token = get_decoded_token(token)
+    partner_id = frappe.db.exists(
+        "Sales Partner", {"le_firebase_uid": decoded_token["uid"]}
+    )
+    if not partner_id:
+        frappe.throw(frappe._("Sales Partner does not exist on backend"))
+
+    get_conditions = compose(lambda x: " AND ".join(x), filter(None))
+    conditions = get_conditions(
+        [
+            "docstatus < 2",
+            "sales_partner = %(sales_partner)s",
+            "workflow_state = %(workflow_state)s" if status else None,
+        ]
+    )
+
+    get_count = compose(
+        lambda x: x[0][0],
+        lambda x: frappe.db.sql(
+            """
+                SELECT COUNT(name) FROM `tabDelivery Note`
+                WHERE {conditions}
+            """.format(
+                conditions=conditions
+            ),
+            values={"sales_partner": x, "workflow_state": status},
+        ),
+    )
+
+    delivery_notes = frappe.db.sql(
+        """
+            SELECT
+                name,
+                le_scheduled_datetime AS scheduled_datetime,
+                TIMESTAMP(posting_date, posting_time) AS posting_datetime,
+                customer,
+                customer_name,
+                shipping_address_name,
+                customer_address,
+                rounded_total AS total,
+                workflow_state AS status
+            FROM `tabDelivery Note`
+            WHERE {conditions}
+            ORDER BY le_scheduled_datetime DESC, creation DESC
+            LIMIT %(start)s, %(page_length)s
+        """.format(
+            conditions=conditions
+        ),
+        values={
+            "sales_partner": partner_id,
+            "workflow_state": status,
+            "start": (frappe.utils.cint(page) - 1) * frappe.utils.cint(page_length),
+            "page_length": frappe.utils.cint(page_length),
+        },
+        as_dict=1,
+    )
+    items = (
+        compose(
+            groupby("parent"),
+            lambda names: frappe.db.sql(
+                """
+                    SELECT
+                        dni.parent AS parent,
+                        dni.item_code AS item_code,
+                        dni.item_name AS item_name,
+                        dni.item_group AS item_group,
+                        i.thumbnail AS image,
+                        dni.amount AS amount
+                    FROM `tabDelivery Note Item` AS dni
+                    LEFT JOIN `tabItem` AS i ON i.name = dni.item_code
+                    WHERE dni.parent IN %(parents)s
+                """,
+                values={"parents": names},
+                as_dict=1,
+            ),
+            list,
+            map(lambda x: x.get("name")),
+        )(delivery_notes)
+        if delivery_notes
+        else {}
+    )
+    addresses = compose(
+        valmap(lambda x: x[0]),
+        groupby("name"),
+        lambda names: frappe.db.sql(
+            """
+                    SELECT name, address_line1, address_line2, city, state, country, pincode
+                    FROM `tabAddress`
+                    WHERE name IN %(names)s
+                """,
+            values={"names": names},
+            as_dict=1,
+        )
+        if names
+        else [],
+        list,
+        filter(None),
+        concat,
+        map(lambda x: [x.get("shipping_address_name"), x.get("customer_address")]),
+    )(delivery_notes)
+    return {
+        "count": get_count(partner_id),
+        "items": [
+            merge(
+                pick(
+                    [
+                        "name",
+                        "customer",
+                        "customer_name",
+                        "posting_datetime",
+                        "scheduled_datetime",
+                        "total",
+                        "status",
+                    ],
+                    x,
+                ),
+                {
+                    "items": items.get(x.get("name"), []),
+                    "address": addresses.get(
+                        x.get("shipping_address_name") or x.get("customer_address")
+                    ),
+                },
+            )
+            for x in delivery_notes
+        ],
+    }
+
+
+@frappe.whitelist(allow_guest=True)
+@handle_error
+def act_on_job(token, note_name, action, posting_datetime=None):
+    decoded_token = get_decoded_token(token)
+    partner_id = frappe.db.exists(
+        "Sales Partner", {"le_firebase_uid": decoded_token["uid"]}
+    )
+    if not partner_id:
+        frappe.throw(frappe._("Partner does not exist on backend"))
+
+    session_user = frappe.session.user
+    settings = frappe.get_single("Leiteng Website Settings")
+    if not settings.user:
+        frappe.throw(frappe._("Site setup not complete"))
+
+    frappe.set_user(settings.user)
+    doc = frappe.get_doc("Delivery Note", note_name)
+    if not doc:
+        frappe.throw(frappe._("Document #{} does not exists".format(note_name)))
+    if doc.workflow_state != "Pending":
+        frappe.throw(
+            frappe._("Action cannot be performed on document #{}".format(note_name))
+        )
+
+    frappe.model.workflow.apply_workflow(doc, action)
+    frappe.set_user(session_user)
+    return merge(
+        pick(["name", "posting_date", "posting_time"], doc.as_dict()),
+        {"status": doc.workflow_state},
+    )
