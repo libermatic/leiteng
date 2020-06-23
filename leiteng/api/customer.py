a435cd44cebf41c321ff00d55d21d182a58160ab
Sun Howwrongbum <sun@libermatic.com>
2020-06-23 11:24:05 +0000

    feat: send tax

--- leiteng/api/customer.py
+++ leiteng/api/customer.py
@@ -227,7 +227,9 @@ def list_orders(token, page="1", page_length="10", status=None):
 
     orders = frappe.db.sql(
         """
-            SELECT name, transaction_date, rounded_total, status
+            SELECT
+                name, transaction_date, status,
+                total, total_taxes_and_charges, grand_total, rounding_adjustment, rounded_total
             FROM `tabSales Order`
             WHERE {conditions}
             ORDER BY transaction_date DESC, creation DESC
@@ -288,6 +290,10 @@ def get_notes(token, so_name):
                 "partner_name",
                 "scheduled_datetime",
                 "posting_datetime",
+                "total",
+                "total_taxes_and_charges",
+                "grand_total",
+                "rounding_adjustment",
                 "rounded_total",
                 "status",
             ]
@@ -325,6 +331,10 @@ def get_notes(token, so_name):
                     sp.partner_name,
                     dn.le_scheduled_datetime AS scheduled_datetime,
                     TIMESTAMP(dn.posting_date, dn.posting_time) AS posting_datetime,
+                    dn.total,
+                    dn.total_taxes_and_charges,
+                    dn.grand_total,
+                    dn.rounding_adjustment,
                     dn.rounded_total,
                     dn.workflow_state AS status,
                     dni.name AS child_name,
@@ -352,6 +362,23 @@ def get_notes(token, so_name):
 
 @frappe.whitelist(allow_guest=True)
 @handle_error
+def make_order(token, **kwargs):
+    customer_id = _get_customer_id(token)
+
+    session_user = frappe.session.user
+    settings = frappe.get_single("Leiteng Website Settings")
+    if not settings.user:
+        frappe.throw(frappe._("Site setup not complete"))
+    frappe.set_user(settings.user)
+
+    doc = _make_sales_order(customer_id, **kwargs)
+    doc.calculate_taxes_and_totals()
+    frappe.set_user(session_user)
+    return _get_formatted_sales_order(doc)
+
+
+@frappe.whitelist(allow_guest=True)
+@handle_error
 def create_order(token, **kwargs):
     decoded_token = get_decoded_token(token)
     customer_id = frappe.db.exists(
@@ -366,6 +393,36 @@ def create_order(token, **kwargs):
         frappe.throw(frappe._("Site setup not complete"))
     frappe.set_user(settings.user)
 
+    doc = _make_sales_order(customer_id, **kwargs)
+    doc.insert()
+    doc.submit()
+    frappe.set_user(session_user)
+    return _get_formatted_sales_order(doc)
+
+
+@frappe.whitelist(allow_guest=True)
+@handle_error
+def create_messaging_registration(token, registration_token):
+    customer_id = _get_customer_id(token)
+
+    session_user = frappe.session.user
+    settings = frappe.get_single("Leiteng Website Settings")
+    if not settings.user:
+        frappe.throw(frappe._("Site setup not complete"))
+    frappe.set_user(settings.user)
+
+    frappe.db.set_value("Customer", customer_id, "le_fcm_token", registration_token)
+
+    frappe.set_user(session_user)
+
+
+@frappe.whitelist(allow_guest=True)
+@handle_error
+def remove_messaging_registration(token):
+    create_messaging_registration(token, None)
+
+
+def _make_sales_order(customer_id, **kwargs):
     args = pick(["transaction_date", "delivery_date", "customer_address"], kwargs)
 
     doc = frappe.get_doc(
@@ -401,47 +458,45 @@ def create_order(token, **kwargs):
         )
 
     doc.set_missing_values()
-    doc.insert()
-    doc.submit()
-    frappe.set_user(session_user)
+    return doc
+
+
+def _get_formatted_sales_order(doc):
     return merge(
         pick(
-            ["name", "transaction_date", "delivery_date", "rounded_total"],
+            [
+                "name",
+                "transaction_date",
+                "delivery_date",
+                "total",
+                "grand_total",
+                "rounding_adjustment",
+                "rounded_total",
+            ],
             doc.as_dict(),
         ),
         {
             "delivery_time": doc.le_delivery_time,
             "items": [
                 pick(
-                    ["item_code", "item_name", "item_group", "qty", "rate", "amount"],
+                    [
+                        "item_code",
+                        "item_name",
+                        "item_group",
+                        "qty",
+                        "rate",
+                        "amount",
+                        "net_amount",
+                    ],
                     x.as_dict(),
                 )
                 for x in doc.items
             ],
+            "taxes": [
+                pick(["description", "tax_amount"], x.as_dict()) for x in doc.taxes
+            ],
         },
     )
-
-
-@frappe.whitelist(allow_guest=True)
-@handle_error
-def create_messaging_registration(token, registration_token):
-    customer_id = _get_customer_id(token)
-
-    session_user = frappe.session.user
-    settings = frappe.get_single("Leiteng Website Settings")
-    if not settings.user:
-        frappe.throw(frappe._("Site setup not complete"))
-    frappe.set_user(settings.user)
-
-    frappe.db.set_value("Customer", customer_id, "le_fcm_token", registration_token)
-
-    frappe.set_user(session_user)
-
-
-@frappe.whitelist(allow_guest=True)
-@handle_error
-def remove_messaging_registration(token):
-    create_messaging_registration(token, None)
 
 
 def _get_customer_id(token):
